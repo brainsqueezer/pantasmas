a6ddbe79ab433bc27dfadb3007b71938138f1a26
Ramon A. Parada <ramon@bigpress.net>
2016-11-12 13:50:31 +0100

    share icons

---
+++ explore.php
@@ -0,0 +1,13 @@
+
+
+
+<div class="txt_center">
+    <ul class="list_redes">
+        <li><a href="https://www.facebook.com/sharer.php?u=http://pantasmas.historiable.com/explore.php" target="_blank"><img src="/images/share/ico_facebook.png"></a></li>
+        <li><a href="https://twitter.com/intent/tweet?url=http://pantasmas.historiable.com/explore.php" target="_blank"><img src="/images/share/ico_twitter.png"></a></li>
+        <li><a href="https://plus.google.com/share?url=http://pantasmas.historiable.com/explore.php" target="_blank"><img src="/images/share/ico_googleplus.png"></a></li>
+        <li><a href="http://pinterest.com/pin/create/link/?url=http://pantasmas.historiable.com/explore.php" target="_blank"><img src="/images/share/ico_pinterest.png"></a></li>
+    </ul>
+</div>
+
+
